3e80d6a77aacfee1c470acf1f9816899dc01ece2
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2019-11-07 15:10:58 +0300

    Update TextEditor.java

--- TextEditor.java
+++ TextEditor.java
@@ -2,36 +2,79 @@ package editor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class TextEditor extends JFrame {
-    private void initTextArea() {
-        JTextArea textArea = new JTextArea();
-        JScrollPane scrollPane = new JScrollPane(textArea);
+    private JTextField textField;
+    private JTextArea textArea;
 
-        textArea.setLineWrap(true);
-        textArea.setWrapStyleWord(true);
+    private void init() {
+        getContentPane().setLayout(null);
 
-        Container pane = getContentPane();
-        GroupLayout groupLayout = new GroupLayout(pane);
-        pane.setLayout(groupLayout);
+        textField = new JTextField();
+        textField.setBounds(1, 0, 268, 39);
+        getContentPane().add(textField);
+        textField.setColumns(10);
+        textField.setName("FilenameField");
 
-        groupLayout.setAutoCreateContainerGaps(true);
-        groupLayout.setAutoCreateGaps(true);
+        Button buttonSave = new Button("Save");
+        buttonSave.setBounds(354, 0, 80, 39);
+        getContentPane().add(buttonSave);
+        buttonSave.setName("SaveButton");
 
-        groupLayout.setHorizontalGroup(groupLayout.createParallelGroup().addComponent(scrollPane));
-        groupLayout.setVerticalGroup(groupLayout.createSequentialGroup().addComponent(scrollPane));
-        pack();
+        Button buttonLoad = new Button("Load");
+        buttonLoad.setBounds(268, 0, 88, 39);
+        getContentPane().add(buttonLoad);
+        buttonLoad.setName("LoadButton");
 
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(1, 50, 433, 211);
+        getContentPane().add(scrollPane);
+        scrollPane.setName("ScrollPane");
+
+        textArea = new JTextArea();
+        scrollPane.setViewportView(textArea);
         textArea.setName("TextArea");
+
+        buttonSave.addActionListener(this::writeToFile);
+        buttonLoad.addActionListener(this::readFile);
+    }
+
+    private void writeToFile(ActionEvent e) {
+        String text = textField.getText();
+        try (PrintWriter out = new PrintWriter(
+                text.endsWith(".txt") ?
+                        text : text + ".txt")) {
+            out.println(textArea.getText());
+        } catch (FileNotFoundException error) {
+            System.out.println("No permission to save into this directory.");
+        }
+    }
+
+    private void readFile(ActionEvent e) {
+        try {
+            String content = Files.readString(Paths.get("file.txt"));
+            textArea.setText(content);
+        } catch (IOException error) {
+            System.out.println("No such file exists.");
+            textArea.setText("No such file exists.");
+        }
+
     }
 
     public TextEditor() {
-        initTextArea();
+        init();
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
+        setSize(450, 300);
         setVisible(true);
         setLayout(null);
-        setTitle("The first stage");
+        setResizable(false);
+        setTitle("Text Editor");
     }
 }
