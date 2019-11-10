e28640c59f84bceb59f11e10d91604bfb924859d
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2019-11-10 17:16:42 +0300

    Create SaveFileCommand.java

---
+++ SaveFileCommand.java
@@ -0,0 +1,28 @@
+package editor;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class SaveFileCommand extends Command {
+    SaveFileCommand(TextEditor editor) {
+        super(editor);
+    }
+
+    @Override
+    public void execute() {
+        int returnValue = textEditor.chooser.showSaveDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = textEditor.chooser.getSelectedFile();
+            String path = selectedFile.getAbsolutePath();
+            try (PrintWriter out = new PrintWriter(
+                    path.endsWith(".txt") ?
+                            path : path + ".txt")) {
+                out.println(textEditor.textArea.getText());
+            } catch (FileNotFoundException error) {
+                System.out.println("No permission to save into this directory.");
+            }
+        }
+    }
+}
