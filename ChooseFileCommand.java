a819090a43f21abdc3bb248a4f16b3df245924de
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2019-11-10 14:24:16 +0300

    Create ChooseFileCommand.java

---
+++ ChooseFileCommand.java
@@ -0,0 +1,31 @@
+package editor;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ChooseFileCommand extends Command {
+    ChooseFileCommand(TextEditor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        int returnValue = textEditor.chooser.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = textEditor.chooser.getSelectedFile();
+            String text = selectedFile.getAbsolutePath();
+            try {
+                String content = Files.readString(Paths.get(text.endsWith(".txt") ?
+                        text : text + ".txt"));
+                System.out.println(content);
+                textEditor.textArea.setText((content.substring(0, content.length() - 2)));
+            } catch (IOException e) {
+                textEditor.textArea.setText("");
+            }
+        }
+        return false;
+    }
+}
