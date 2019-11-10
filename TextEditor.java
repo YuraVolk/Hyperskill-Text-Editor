8834266f10bd3c80a06be5a3dbb63db9e68c92c3
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2019-11-10 13:06:17 +0300

    Update TextEditor.java

--- TextEditor.java
+++ TextEditor.java
@@ -13,22 +13,6 @@ public class TextEditor extends JFrame {
 
     private void init() {
         getContentPane().setLayout(null);
-        textField = new JTextField();
-        textField.setBounds(10, 41, 259, 21);
-        getContentPane().add(textField);
-        textField.setColumns(10);
-        textField.setName("FilenameField");
-
-        JButton buttonSave = new JButton("Save");
-        buttonSave.setBounds(353, 41, 80, 21);
-        getContentPane().add(buttonSave);
-        buttonSave.setName("SaveButton");
-
-        JButton buttonLoad = new JButton("Load");
-        buttonLoad.setBounds(267, 41, 88, 21);
-        getContentPane().add(buttonLoad);
-        buttonLoad.setName("LoadButton");
-
         JScrollPane scrollPane = new JScrollPane();
         scrollPane.setBounds(11, 65, 423, 201);
         getContentPane().add(scrollPane);
@@ -38,14 +22,14 @@ public class TextEditor extends JFrame {
         scrollPane.setViewportView(textArea);
         textArea.setName("TextArea");
 
-        JPanel panel = new JPanel();
-        panel.setBounds(10, 13, 420, 24);
-        getContentPane().add(panel);
-        panel.setLayout(null);
+        JPanel panelMenu = new JPanel();
+        panelMenu.setBounds(0, 0, 444, 26);
+        getContentPane().add(panelMenu);
+        panelMenu.setLayout(null);
 
         JMenuBar menuBar = new JMenuBar();
-        menuBar.setBounds(0, 0, 420, 24);
-        panel.add(menuBar);
+        menuBar.setBounds(0, 0, 444, 24);
+        panelMenu.add(menuBar);
 
         JMenu mnFile = new JMenu("File");
         menuBar.add(mnFile);
@@ -59,16 +43,72 @@ public class TextEditor extends JFrame {
         mnFile.add(mntmLoad);
         mntmLoad.setName("MenuLoad");
 
-        JSeparator separator = new JSeparator();
-        mnFile.add(separator);
+        JSeparator separatorMenu = new JSeparator();
+        mnFile.add(separatorMenu);
 
         JMenuItem mntmExit = new JMenuItem("Exit");
         mnFile.add(mntmExit);
         mntmExit.setName("MenuExit");
 
-        buttonSave.addActionListener(event -> writeToFile());
+        JMenu mnSearch = new JMenu("Search");
+        menuBar.add(mnSearch);
+
+        JMenuItem mntmStartSearch = new JMenuItem("Start Search");
+        mnSearch.add(mntmStartSearch);
+
+        JMenuItem mntmPreviousSearch = new JMenuItem("Previous search");
+        mnSearch.add(mntmPreviousSearch);
+
+        JMenuItem mntmNextMatch = new JMenuItem("Next Match");
+        mnSearch.add(mntmNextMatch);
+
+        JMenuItem mntmUseRegularExpressions = new JMenuItem("Use Regular Expressions");
+        mnSearch.add(mntmUseRegularExpressions);
+
+        JSeparator separator = new JSeparator();
+        separator.setBounds(81, 24, 1, 2);
+        getContentPane().add(separator);
+
+        JPanel panel = new JPanel();
+        panel.setBounds(11, 28, 423, 28);
+        getContentPane().add(panel);
+        panel.setLayout(null);
+
+        JButton btnA = new JButton("");
+        btnA.setIcon(null);
+
+        btnA.setBounds(0, 0, 28, 28);
+        panel.add(btnA);
+
+        JButton btnB = new JButton("");
+
+        btnB.setBounds(32, 0, 28, 28);
+        panel.add(btnB);
+
+        textField = new JTextField();
+        textField.setBounds(67, 4, 187, 20);
+        panel.add(textField);
+        textField.setColumns(10);
+
+        JButton button = new JButton("");
+        button.setBounds(258, 0, 28, 28);
+        panel.add(button);
+
+        JButton button_1 = new JButton("");
+        button_1.setBounds(294, 0, 28, 28);
+        panel.add(button_1);
+
+        JCheckBox chckbxNewCheckBox = new JCheckBox("Use regex");
+        chckbxNewCheckBox.setBounds(325, 3, 92, 23);
+        panel.add(chckbxNewCheckBox);
+
+        JSeparator separator_1 = new JSeparator();
+        separator_1.setBounds(59, 65, 1, 2);
+        getContentPane().add(separator_1);
+
         mntmSave.addActionListener(event -> writeToFile());
-        buttonLoad.addActionListener(event -> readFile());
+        mntmSave.addActionListener(event -> writeToFile());
+        mntmLoad.addActionListener(event -> readFile());
         mntmLoad.addActionListener(event -> readFile());
         mntmExit.addActionListener(event -> exit());
     }
