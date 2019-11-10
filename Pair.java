676742e3c20e7c6ff54ffe62f70367eaf2e582cb
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2019-11-10 17:16:25 +0300

    Create Pair.java

---
+++ Pair.java
@@ -0,0 +1,13 @@
+package editor;
+
+import java.util.ArrayList;
+
+class Pair {
+    ArrayList<Integer> startIndexes;
+    ArrayList<Integer> endIndexes;
+
+    Pair() {
+        startIndexes = new ArrayList<>();
+        endIndexes = new ArrayList<>();
+    }
+}
